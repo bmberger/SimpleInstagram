b8907d97fb57552a2938c394ac93cd202a7b9c09
Briana Berger <brianamb@fb.com>
2019-07-08 14:57:32 -0700

    Added a bottom navigation bar

--- SimpleInstagram/app/src/main/java/com/example/simpleinstagram/LoginActivity.java
+++ SimpleInstagram/app/src/main/java/com/example/simpleinstagram/LoginActivity.java
@@ -24,7 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        launchApp();
         usernameView = (EditText) findViewById(R.id.ivUsernameInput);
         passwordView = (EditText) findViewById(R.id.ivPasswordInput);
     }
